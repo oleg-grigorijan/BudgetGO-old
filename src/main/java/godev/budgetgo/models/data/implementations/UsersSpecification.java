7d1b2b7b66c6b502c0d63d2b18a3845b292ee3ff
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-09-12 20:35:56 +0300

    New User specification by Storage access is added

--- src/main/java/godev/budgetgo/models/data/implementations/UsersSpecification.java
+++ src/main/java/godev/budgetgo/models/data/implementations/UsersSpecification.java
@@ -5,6 +5,7 @@ import godev.budgetgo.models.data.Specification;
 public class UsersSpecification implements Specification<User> {
     private Long id;
     private String email;
+    private Storage storage;
 
     public UsersSpecification whereId(Long id) {
         this.id = id;
@@ -24,9 +25,19 @@ public class UsersSpecification implements Specification<User> {
         return email;
     }
 
+    public UsersSpecification withStorageAccess(Storage storage) {
+        this.storage = storage;
+        return this;
+    }
+
+    public Storage getStorage() {
+        return storage;
+    }
+
     @Override
     public boolean specified(User entity) {
         return (id == null || entity.getId() == id)
-                && (email == null || entity.getEmail().equals(email));
+                && (email == null || entity.getEmail().equals(email))
+                && (storage == null || storage.getUsers().contains(entity));
     }
 }
--- src/test/java/godev/budgetgo/models/data/UsersSpecificationTest.java
+++ src/test/java/godev/budgetgo/models/data/UsersSpecificationTest.java
@@ -1,8 +1,6 @@
 package godev.budgetgo.models.data;
 
-import godev.budgetgo.models.data.implementations.User;
-import godev.budgetgo.models.data.implementations.UserBuilder;
-import godev.budgetgo.models.data.implementations.UsersSpecification;
+import godev.budgetgo.models.data.implementations.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -14,16 +12,27 @@ class UsersSpecificationTest {
         long id = 1;
         String email = "oleg.grigorijan@gmail.com";
 
-        Specification<User> specification = new UsersSpecification()
+        UsersSpecification specification = new UsersSpecification()
                 .whereId(id)
                 .whereEmail(email);
 
-        User correctUser = new UserBuilder().setId(id).setEmail(email).create();
+
+        User correctUser = new UserBuilder().setId(id).setEmail(email).setName("SomeName").create();
         User[] incorrectUsers = {
                 new UserBuilder().setId(id + 1).setEmail(email).create(),
                 new UserBuilder().setId(id).setEmail("incorrect" + email).create(),
-                new UserBuilder().create()
+                new UserBuilder().create(),
+                new UserBuilder().setId(id).setEmail(email).setName("AnotherName").create(),
         };
+
+        Storage storage = new StorageBuilder()
+                .addUser(correctUser)
+                .addUser(incorrectUsers[0])
+                .addUser(incorrectUsers[1])
+                .addUser(incorrectUsers[2])
+                .create();
+
+        specification.withStorageAccess(storage);
 
         assertTrue(specification.specified(correctUser));
         for (User u : incorrectUsers) assertFalse(specification.specified(u));
